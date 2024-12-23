d633d72f67959d954fd01685bb5c060f291eae93
ARKCODEsa <arqjhefferson@hotmail.com>
2024-12-23 02:36:50 -0500

    Proyecto terminado, falta readme.txt o .md incluido su index.md

---
+++ src/main/java/com/arkcode/proyecto_de_simulacion/launcher.java
@@ -0,0 +1,9 @@
+package com.arkcode.proyecto_de_simulacion;
+
+import javafx.application.Application;
+
+public class launcher {
+    public static void main(String[] args) {
+        Application.launch(App.class, args);
+    }
+}
